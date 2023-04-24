63d0c6675ff52ad27e2cab9b5ee577ebb6cc236d
Ángel Palacios López <118205147+angelpl16@users.noreply.github.com>
2023-04-24 20:30:59 +0200

    Archivos Iniciales

---
+++ src/lsi/ubu/solucion/GestionMedicos.java
@@ -0,0 +1,21 @@
+package lsi.ubu.solucion;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+public class GestionMedicos {
+	public static void reservar_consulta(String m_NIF_cliente, String m_NIF_medico, Date m_Fecha_Consulta)
+			throws SQLException {
+
+	}
+
+	public static void anular_consulta(String m_NIF_cliente, String m_NIF_medico, Date m_Fecha_Consulta,
+			Date m_Fecha_Anulacion, String motivo) throws SQLException {
+
+	}
+
+	public static void consulta_medico(String m_NIF_medico) throws SQLException {
+
+	}
+
+}
